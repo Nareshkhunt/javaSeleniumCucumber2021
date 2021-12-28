ec146d70c4717aac163a953bc2caa8177a194397
Nareshkhunt <47598866+Nareshkhunt@users.noreply.github.com>
2021-12-28 15:26:44 +0000

    Update DriverManager.java

--- src/test/java/com/resourceWise/utility/DriverManager.java
+++ src/test/java/com/resourceWise/utility/DriverManager.java
@@ -40,13 +40,13 @@ public DriverManager(){
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless");
-                options.addArguments("start-maximized"); // open Browser in maximized mode
-                options.addArguments("disable-infobars"); // disabling infobars
-                options.addArguments("--disable-extensions"); // disabling extensions
-                options.addArguments("--disable-gpu"); // applicable to windows os only
-                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                options.addArguments("--no-sandbox"); // Bypass OS security model
+                options.setHeadless(true);
+//                 options.addArguments("start-maximized"); // open Browser in maximized mode
+//                 options.addArguments("disable-infobars"); // disabling infobars
+//                 options.addArguments("--disable-extensions"); // disabling extensions
+//                 options.addArguments("--disable-gpu"); // applicable to windows os only
+//                 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//                 options.addArguments("--no-sandbox"); // Bypass OS security model
                 driver = new ChromeDriver(options);
 //                 driver=new ChromeDriver();
                 break;
